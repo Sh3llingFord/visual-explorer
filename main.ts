73b3f0455959be9eb71f7a0fa78aac488fd95763
Andreas Langer <a.langer@inmc.info>
2026-06-08 16:38:25 +0200

    1.0.6: Fine-tune back button position

--- main.ts
+++ main.ts
@@ -725,7 +725,7 @@ export default class NoteGalleryPlugin extends Plugin {
       }
       .note-gallery-back-btn {
         position: absolute;
-        bottom: 21px;
+        bottom: 22px;
         left: 16px;
         width: 50px;
         height: 50px;
